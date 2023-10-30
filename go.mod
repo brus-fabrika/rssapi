6ddac402f589b92c42b6fe09a0631abf705cd750
Andrey Brus <andrey@px.com>
2023-10-30 22:51:49 +0100

    added basic db setup

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/brus-fabrika/rssapi
 
 go 1.21.3
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
+)
--- main.go
+++ main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
+)
+
+// docker run --rm -v "$((Get-Item .).FullName):/src" -w /src sqlc/sqlc generate
+
+const (
+	DB_USER     = "root"
+	DB_PASSWORD = "root"
+	DB_NAME     = "gotest"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	// Get the PORT environment variable
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT environment is not set")
+	}
+
+	log.Println("PORT is set to", portString)
+
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
+		DB_USER, DB_PASSWORD, DB_NAME)
+	db, err := sql.Open("postgres", dbinfo)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+	log.Println("Database connection established")
+
+	stmt, err := db.Prepare("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = stmt.Exec()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Database table created")
 }

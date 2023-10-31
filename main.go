416e4da0c8b81bf6cdf5160a21cec25cf9397465
Andrey Brus <andrey@px.com>
2023-10-31 18:36:55 +0100

    added some basic http handler to test full cycle with DB

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
+	"encoding/json"
 	"log"
+	"net/http"
 	"os"
+	"time"
 
+	"github.com/brus-fabrika/rssapi/internal/database"
+	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 
 	_ "github.com/lib/pq"
@@ -13,11 +17,9 @@ import (
 
 // docker run --rm -v "$((Get-Item .).FullName):/src" -w /src sqlc/sqlc generate
 
-const (
-	DB_USER     = "root"
-	DB_PASSWORD = "root"
-	DB_NAME     = "gotest"
-)
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 
@@ -29,11 +31,14 @@ func main() {
 		log.Fatal("PORT environment is not set")
 	}
 
+	dbConnString := os.Getenv("DB_URL")
+	if dbConnString == "" {
+		log.Fatal("Postgress connection string is not set")
+	}
+
 	log.Println("PORT is set to", portString)
 
-	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
-		DB_USER, DB_PASSWORD, DB_NAME)
-	db, err := sql.Open("postgres", dbinfo)
+	db, err := sql.Open("postgres", dbConnString)
 
 	if err != nil {
 		log.Fatal(err)
@@ -42,15 +47,47 @@ func main() {
 	defer db.Close()
 	log.Println("Database connection established")
 
-	stmt, err := db.Prepare("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)")
-	if err != nil {
-		log.Fatal(err)
+	apiCfg := apiConfig{
+		DB: database.New(db),
 	}
 
-	_, err = stmt.Exec()
-	if err != nil {
-		log.Fatal(err)
+	// Start the server
+	log.Println("Starting server on port", portString)
+	http.HandleFunc("/users/create", apiCfg.handlerCreateUser)
+
+	// start the server
+	http.ListenAndServe(":"+portString, nil)
+
+}
+
+func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Name string `json:"name"`
 	}
 
-	log.Println("Database table created")
+	decoder := json.NewDecoder(r.Body)
+
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	log.Println("Creating user with name:", params.Name)
+
+	user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID:        uuid.New(),
+		Name:      params.Name,
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+	})
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user)
 }
